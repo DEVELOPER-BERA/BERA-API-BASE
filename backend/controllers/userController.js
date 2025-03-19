162a71b6bde46fd94b8498e6b72ac7057cae721c
DEVELOPER-BERA <berabruce9@gmail.com>
2025-03-19 15:20:28 +0300

    Create userController.js

---
+++ backend/controllers/userController.js
@@ -0,0 +1,11 @@
+const db = require('../config/db');
+
+exports.getUsers = (req, res) => {
+    db.query('SELECT * FROM users', (err, results) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+        } else {
+            res.json(results);
+        }
+    });
+};
