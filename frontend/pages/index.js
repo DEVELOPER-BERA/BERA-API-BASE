1c19bec77346a683a23f4c23695ccb97ca1a8586
DEVELOPER-BERA <berabruce9@gmail.com>
2025-03-19 14:46:30 +0300

    Update index.js

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,12 +1,3 @@
-import Navbar from '../components/Navbar';
-
 export default function Home() {
-  return (
-    <div>
-      <Navbar />
-      <h1 className="text-3xl font-bold text-center mt-10">
-        Welcome to BERA TECH API BASE
-      </h1>
-    </div>
-  );
+  return <h1>Welcome to BERA TECH API BASE</h1>;
 }
